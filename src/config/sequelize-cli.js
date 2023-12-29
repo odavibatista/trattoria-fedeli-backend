3f26150002dbcb94563f06e77d6caa4213df49e8
Davi Batista <davideosmar11@gmail.com>
2023-12-29 18:36:23 -0300

    feat: changed informations on config file

--- src/config/sequelize-cli.js
+++ src/config/sequelize-cli.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-    db_je3k: {
-        url: process.env.DATABASE_URL,
-        dialect: 'postgres'
-    },
-
     development: {
-        url: process.env.DATABASE_URL,
-        dialect: 'postgres'
-    },
+        "url": process.env.DATABASE_URL,
+        "dialect": 'postgres',
+        "dialectOptions": {
+            "ssl": {
+                "require": true,
+                "rejectUnauthorized": false
+            }
+        }
+    }
 }
